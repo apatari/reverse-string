ad2510167c9a6b554c0b3899779f60c86ac0d439
Andy Patari <andy.patari@gmail.com>
2023-09-11 21:03:03 -0400

    passing tests

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let res = ""
+  for(let i = str.length - 1; i >=0; i-- ) {
+    res += str[i]
+  }
+  return res
 }
 
 if (require.main === module) {
@@ -16,4 +21,11 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+// initialize an empty result string
+// iterate over the provided string backwards, adding each char to the result
+
+
 // And a written explanation of your solution
+// The function creates an empty string and then works its way backwards
+// through the provided string, adding each letter to the result.  Once
+// it has made it through the whole string the result string is returned.
